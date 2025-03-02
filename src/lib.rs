2cbc6dd921f8d94f858e9f46f35da13063bf7c62
HaineSensei <81431922+HaineSensei@users.noreply.github.com>
2025-03-02 18:31:25 +0000

    started writing the tokenise method of Tokeniser

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,44 @@
 use unicode_segmentation::UnicodeSegmentation;
 
-pub enum Token<'a> {
-    Word {
-        val: &'a str, 
-        start_pos: usize
-    },
-    LParenthesis {
-        val: &'a str,
-        start_pos: usize
-    },
-    RParenthesis {
-        val: &'a str,
-        start_pos: usize
-    },
-    SymbolString {
-        val: &'a str,
-        start_pos: usize
-    },
-    NewLine {
-        val: &'a str,
-        start_pos: usize
-    },
-    WhiteSpace {
-        val: &'a str,
-        start_pos: usize
-    }
+#[derive(Clone,Copy,Debug)]
+pub enum TokenState {
+    Word,
+    LParenthesis,
+    RParenthesis,
+    SymbolString,
+    NewLine,
+    WhiteSpace,
+}
+use TokenState::*;
+
+pub enum Side {
+    Right,
+    Left
+}
+
+pub struct Token<'a> {
+    state: TokenState,
+    val: &'a str,
+    start_pos: usize
 }
 
 impl<'a> Token<'a> {
     pub fn start(&self) -> usize {
-        match self {
-            &Self::Word { val: _, start_pos } => start_pos,
-            &Self::LParenthesis { val: _, start_pos } => start_pos,
-            &Self::RParenthesis { val: _, start_pos } => start_pos,
-            &Self::SymbolString { val: _, start_pos } => start_pos,
-            &Self::NewLine { val: _, start_pos } => start_pos,
-            &Self::WhiteSpace { val: _, start_pos } => start_pos
-        }
+        self.start_pos
     }
 
     pub fn value(&self) -> &'a str {
-        match self {
-            &Self::Word { val, start_pos: _ } => val,
-            &Self::LParenthesis { val, start_pos: _ } => val,
-            &Self::RParenthesis { val, start_pos: _ } => val,
-            &Self::SymbolString { val, start_pos: _ } => val,
-            &Self::NewLine { val, start_pos: _ } => val,
-            &Self::WhiteSpace { val, start_pos: _ } => val
-        }
+        self.val
+    }
+
+    pub fn get_state(&self) -> TokenState {
+        self.state
     }
 }
 
 pub struct Tokeniser<'a, 'b, 'c, 'd, 'e> {
     special_characters: Vec<&'a str>,
-    parenthesis_pairs: Vec<(&'b str,&'b str)>,
+    parenthesis_pairs: Vec<(&'b str, &'b str)>,
     single_line_comment: Option<&'c str>,
     multi_line_comment: Option<(&'d str, &'e str)>
 }
@@ -95,6 +80,151 @@ impl<'a, 'b, 'c, 'd, 'e> Tokeniser<'a, 'b, 'c, 'd, 'e> {
     pub fn ml_comment(&self) -> Option<(&'d str, &'e str)> {
         self.multi_line_comment
     }
+
+    pub fn special<'f>(&self, c:&'f str) -> bool {
+        for x in self.specials() {
+            if x == c {
+                return true;
+            }
+        }
+        false
+    }
+    
+    pub fn parenthesis<'f>(&self, c: &'f str) -> Option<Side> {
+        for (x,y) in self.parentheses() {
+            if x == c {
+                return Some(Side::Left);
+            } 
+            if y == c {
+                return Some(Side::Right)
+            }
+        }
+        None
+    }
+
+    pub fn is_sl_comment_start(&self, s: &String) -> bool {
+        match self.sl_comment() {
+            None => false,
+            Some(sl_comment) => s.as_str() == sl_comment
+        }
+    }
+
+    pub fn is_ml_comment_start(&self, s: &String) -> bool {
+        match self.ml_comment() {
+            None => false,
+            Some((start,_)) => s.as_str() == start
+        }
+    }
+
+    pub fn is_ml_comment_end(&self, s: &String) -> bool {
+        match self.ml_comment() {
+            None => false,
+            Some((_, end)) => s.as_str() == end
+        }
+    }
+
+    pub fn well_formed(&self) -> bool {
+        for (x,y) in self.parentheses() {
+            if (!self.special(x)) || !self.special(y) {
+                return false;
+            }
+        }
+        if let Some(sl_comment) = self.sl_comment() {
+            for c in sl_comment.graphemes(true) {
+                if !self.special(c) {
+                    return false;
+                }
+            }
+        }
+        if let Some((start, end)) = self.ml_comment() {
+            for c in start.graphemes(true) {
+                if !self.special(c) {
+                    return false;
+                }
+            }
+            for c in end.graphemes(true) {
+                if !self.special(c) {
+                    return false
+                }
+            }
+        }
+        true
+    }
+    /// TODO: Add whitespace and comment handling
+    pub fn tokenise<'f>(&self, text: &'f str) -> Result<Vec<Token<'f>>,String> {
+        let mut out: Vec<Token<'f>> = Vec::new();
+        let mut curr_string: String = String::new();
+        let mut curr_start: usize = 0;
+        let mut curr_state : Option<TokenState> = None;
+        for (curr_pos,c) in text.grapheme_indices(true) {
+            match curr_state {
+                None => {
+                    curr_string.push_str(c);
+                    if self.special(c) {
+                        curr_state = Some(SymbolString);
+                        curr_start = curr_pos;
+                        match self.parenthesis(c) {
+                            Some(Side::Left) => {
+                                out.push(Token { state: LParenthesis, val: c, start_pos: curr_pos });
+                                curr_string = String::new();
+                                curr_state = None;
+                            },
+                            Some(Side::Right) => {
+                                out.push(Token { state: RParenthesis, val: c, start_pos: curr_pos });
+                                curr_string = String::new();
+                                curr_state = None;
+                            },
+                            None => {}
+                        }
+                    } else {
+                        curr_state = Some(Word);
+                        curr_start = curr_pos;
+                    }
+                },
+                Some(Word) => {
+                    if self.special(c) {
+                        out.push(Token{
+                            state: Word,
+                            val: &text[curr_start..curr_pos],
+                            start_pos: curr_start
+                        });
+                        
+                        match self.parenthesis(c) {
+                            Some(Side::Left) => {
+                                out.push(Token { state: LParenthesis, val: c, start_pos: curr_pos });
+                                curr_string = String::new();
+                                curr_state = None;
+                            },
+                            Some(Side::Right) => {
+                                out.push(Token { state: RParenthesis, val: c, start_pos: curr_pos });
+                                curr_string = String::new();
+                                curr_state = None;
+                            },
+                            None => {
+                                curr_string = String::from(c);
+                                curr_start = curr_pos;
+                                curr_state = Some(SymbolString);
+                            }
+                        }
+                    } else {
+                        curr_string.push_str(c);
+                    }
+                },
+                Some(SymbolString) => {
+                    if !self.special(c) {
+                        out.push(Token { state: SymbolString, val: &text[curr_start..curr_pos], start_pos: curr_start });
+                        curr_string = String::from(c);
+                        curr_start = curr_pos;
+                        if is_whitespace(c) {
+
+                        }
+                    }
+                },
+                other => {return Err(format!("curr_state should never reach {:?}",other))}
+            }
+        }
+        Ok(out)
+    }
 }
 
 #[cfg(test)]
@@ -103,7 +233,7 @@ mod tests {
 
     #[test]
     fn token_gets_work() {
-        let token = Token::Word {val:"hi", start_pos:4};
+        let token = Token{ state: Word, val:"hi", start_pos:4};
         assert_eq!(token.start(), 4);
         assert_eq!(token.value(), "hi");
     }
